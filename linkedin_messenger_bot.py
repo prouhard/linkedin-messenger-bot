53fff629d5cb47d753c23b76d80dd96f18082835
Reupiey <p.rouhard@hotmail.fr>
2018-08-30 16:06:40 +0200

    (WIP) Await for new messages

--- linkedin_messenger_bot.py
+++ linkedin_messenger_bot.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 
+from copy import deepcopy
+
 import json
 import os
 import time
@@ -22,23 +24,38 @@ class LinkedinMessengerBot:
 
     def go_to_messaging(self):
         self.driver.find_element_by_id('messaging-nav-item').click()
+        time.sleep(5)
+
+    def wait_for_new_conversation(self):
+        cached_top_conversation = self.get_nth_conversation(0)
+        while True:
+            current_top_conversation = self.get_nth_conversation(0)
+            print(current_top_conversation)
+            if cached_top_conversation != current_top_conversation:
+                print('New message !')
+                yield current_top_conversation
 
     def get_nth_conversation(self, n):
-        time.sleep(5)
         conversation_list = self.driver.find_element_by_class_name(
             'msg-conversations-container__conversations-list'
         )
         conversations = conversation_list.find_elements_by_tag_name('li')
         if len(conversations) <= n:
-            return 'This conversation does not exist.'
-        conversations[n].click()
+            raise IndexError('This conversation does not exist.')
+        return conversations[n]
+
+    def get_nth_conversations_messages(self, n):
+        time.sleep(5)
+        self.get_nth_conversation(n).click()
         first_conversation = self.driver.find_element_by_class_name('msg-s-message-list')
         messages = first_conversation.find_elements_by_tag_name('li')[1:]
         for message in messages:
             links = message.find_elements_by_class_name('msg-s-message-group__profile-link')
             if links:
                 name = links[0].get_attribute('href')
-            message = message.find_element_by_class_name('msg-s-event-listitem__message-bubble').find_element_by_tag_name('p').text
+            message = message.find_element_by_class_name(
+                'msg-s-event-listitem__message-bubble'
+            ).find_element_by_tag_name('p').text
             print(name, message)
 
 
@@ -60,7 +77,8 @@ class LinkedinMessengerBot:
     def run(self):
         self.login()
         self.go_to_messaging()
-        self.get_nth_conversation(0)
+        for i in self.wait_for_new_conversation():
+            print(i)
 
 
 if __name__ == '__main__':
